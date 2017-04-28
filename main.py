6eae30a7b85046ba225fd7689e38f3ce4d75dbd3
Sha Li <sliwhu@gmail.com>
2017-04-27 17:26:13 -0700

    resubmission

--- main.py
+++ main.py
@@ -1,30 +1,39 @@
+#303 means url valid, but file exists
+#304 means url valid, file not exist
+#305 means url invalid
+
 import os
 import urllib.request as ur
+import sys
 
 def get_data(url):
+
+    filename=url.split('/')[-1]
+    if os.path.exists(filename):
+        print ("File exists, skipping download")
+        return 303
     try:
-        filename=url.split('/')[-1]
-        if os.path.exists(filename):
-            print ("File exists, skipping download")
-            return False
         file=ur.urlopen(url)        
         with open(filename, 'wb') as output:
             output.write(file.read())
             print (filename, "succesfully downloaded")
-            return True
+            return 304
     except:
-        print ('Invalid url')
-        return False
+        print (sys.exc_info()[1])
+        return 305
 
 def remove_data(url):
+    filename=url.split('/')[-1]
     try:
-        filename=url.split('/')[-1]
+        file=ur.urlopen(url)  
+        
         if os.path.exists(filename):
             os.remove(filename)
             print ("File exists, successfully removed.")
-            return True
+            return 303
         else:
             print ('File does not exist.')
-            return False
+            return 304
     except:
-        print ('Invalid url')
+        print (sys.exc_info()[1])
+        return 305
--- test.py
+++ test.py
@@ -3,25 +3,72 @@ import os
 import urllib.request as ur
 from main import get_data, remove_data
 
-class TestMethods(unitest.TestCase):
+class TestMethods(unittest.TestCase):
 	#test file is present locally
 	def test_LocalPresent(self):
+		print ('test_LocalPresent: ')
+		filename='4xy5-26gy.csv'
+		try:
+			os.remove(filename)
+		except:
+			pass
+		test_url ='https://data.seattle.gov/resource/4xy5-26gy.csv'
+		file=ur.urlopen(test_url)
+		with open(filename, 'wb') as output:
+			output.write(file.read())
+		self.assertEqual(get_data(test_url), 303) 
+		os.remove(filename)
+
+	#test file not present locally
+	def test_LocalnotPresent(self):
+		print ('test_LocalnotPresent: ')
+		filename='4xy5-26gy.csv'
+		try:
+			os.remove(filename)
+		except:
+			pass
+		test_url='https://data.seattle.gov/resource/4xy5-26gy.csv'
+		self.assertEqual(get_data(test_url), 304)
+
+	#test url not valid
+	def test_GeturlInvalid(self):
+		print ('test_GeturlInvalid: ')
+		test_url='http://invalid'
+		self.assertEqual(get_data(test_url), 305)
+	
+
+	#test remove file if file exist
+	def test_RemoveLocalPresent(self):
+		print ('test_RemoveLocalPresent: ')
+		filename='4xy5-26gy.csv'
+		try:
+			os.remove(filename)
+		except:
+			pass
 		test_url='https://data.seattle.gov/resource/4xy5-26gy.csv'
 		file=ur.urlopen(test_url)
-		with open('test.csv', 'wb') as output:
+		with open(filename, 'wb') as output:
 			output.write(file.read())
-		self.assertFalse(get_data(test_url)) 
+		self.assertEqual(remove_data(test_url), 303) 	
+		try:
+			os.remove(filename)
+		except:
+			pass
 
-	def test_LocalnotPresent(self):
+	def test_RemoveLocalnotPresent(self):
+		print ('test_RemoveLocalnotPresent: ')
+		filename='4xy5-26gy.csv'
+		try:
+			os.remove(filename)
+		except:
+			pass
 		test_url='https://data.seattle.gov/resource/4xy5-26gy.csv'
-		self.assertTrue(get_data(test_url))
-
-	def test_urlInvalid(self):
+		self.assertEqual(remove_data(test_url), 304) 
+	
+	def test_RemoveurlInvalid(self):
+		print ('test_RemoveurlInvalid: ')
 		test_url='http://invalid'
-		self.assertFalse(get_data(test_url))
-
-	def test_RemoveExist(self):
-		return
+		self.assertEqual(remove_data(test_url), 305)
 
 if __name__ == '__main__':
     unittest.main()
